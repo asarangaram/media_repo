cc0df7a319f1ad81d9b15604f40e68e9350af0db
anandas <4833102+asarangaram@users.noreply.github.com>
2024-10-22 20:24:39 +0530

    include  get_media_count

--- src/endpoint/collection/schemas.py
+++ src/endpoint/collection/schemas.py
@@ -6,7 +6,7 @@ from marshmallow import Schema, fields, post_dump, validates_schema, ValidationE
 from ..media.media_types import  MillisecondsSinceEpoch
 
 class CollectionSchema(Schema):
-    SKIP_VALUES = set([None])
+    SKIP_VALUES = set([None, ''])
     server_uid = fields.Int(attribute="id", data_key="serverUID", dump_only=True)
     
     label = fields.Str(required=True)
@@ -14,6 +14,11 @@ class CollectionSchema(Schema):
     createdDate = MillisecondsSinceEpoch(dump_only=True)
     updatedDate = MillisecondsSinceEpoch(dump_only=True)
     # media = fields.List(fields.Nested(MediaSchemaGET),  dump_only=True)
+
+    media_count = fields.Method("get_media_count", dump_only=True)
+
+    def get_media_count(self, obj):
+        return len(obj.media)
     
     @post_dump
     def remove_skip_values(self, data, **kwargs):
@@ -30,7 +35,7 @@ class CollectionCreateSchema(Schema):
 class CollectionUpdateSchema(Schema):
     label = fields.Str()
     description = fields.Str()
-    server_uid = fields.Int(attribute="id", data_key="serverUID", dump_only=True)
+    server_uid = fields.Int(attribute="id", data_key="serverUID")
     @validates_schema
     def validate_at_least_one(self, data, **kwargs):
         if not data.get("label") and not data.get("description"):
