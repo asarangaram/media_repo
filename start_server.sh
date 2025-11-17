a20fb7b72c2f9ccb1599f2175c5ae3ba79b67409
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-11-17 17:29:47 +0530

    dont create venv, we use pyenv

--- start_server.sh
+++ start_server.sh
@@ -60,14 +60,14 @@ check_system_software()
     fi
 }
 
-create_venv
+# create_venv
 
-os_name=$(uname)
-if [ "$os_name" == "Darwin" ]; then
-    check_system_software
-fi
+#os_name=$(uname)
+#if [ "$os_name" == "Darwin" ]; then
+#    check_system_software
+#fi
 # dns-sd on macOS / avahi-publish + avahi-browse on Linux
-bash ./broadcast.sh &
+# bash ./broadcast.sh &
 
 # avahi is running as a service
 # vi /etc/systemd/system/avahi.service
